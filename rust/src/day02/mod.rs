fa3b10f1717bb5a8bcf257abf8520e7fd462e637
me <literally@me>
2022-12-02 09:27:07 +0300

    sync

--- rust/src/day02/mod.rs
+++ rust/src/day02/mod.rs
@@ -1,23 +1,5 @@
-fn parse_hand(input: u8) -> i32 {
-  match input {
-    b'A' | b'X' => 0,
-    b'B' | b'Y' => 1,
-    b'C' | b'Z' => 2,
-    _ => panic!(),
-  }
-}
-
-fn parse_shift(input: u8) -> i32 {
-  match input {
-    b'X' => 2,
-    b'Y' => 0,
-    b'Z' => 1,
-    _ => panic!(),
-  }
-}
-
-fn score(me: i32, enemy: i32) -> i32 {
-  match (3 + enemy - me) % 3 {
+fn score(me: u8, enemy: u8) -> u32 {
+  1 + me as u32 + match (3 + enemy - me) % 3 {
     0 => 3,
     1 => 0,
     2 => 6,
@@ -33,14 +15,10 @@ pub fn solve() {
 
   for line in input.lines() {
     let &[lhs, _, rhs] = line.as_bytes() else { panic!() };
-
-    let me = parse_hand(rhs);
-    let enemy = parse_hand(lhs);
-    part1 += 1 + me + score(me, enemy);
-
-    let shift = parse_shift(rhs);
-    let me = (enemy + shift) % 3;
-    part2 += 1 + me + score(me, enemy);
+    let lhs = lhs - b'A';
+    let rhs = rhs - b'X';
+    part1 += score(rhs, lhs);
+    part2 += score((rhs + lhs + 2) % 3, lhs);
   }
 
   println!("day02: {} {}", part1, part2);
