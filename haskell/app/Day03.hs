56e82c4e847ad34192e898f2bab83f097de0e289
me <literally@me>
2022-12-03 19:10:44 +0300

    sync

--- haskell/app/Day03.hs
+++ haskell/app/Day03.hs
@@ -1,20 +1,19 @@
 module Day03 (solve) where
 
 import Data.Char (isAsciiLower, isAsciiUpper, ord)
+import Data.List (intersect)
 import Data.List.Split (chunksOf)
-import Data.Set qualified as S
 import Data.Text qualified as T
 
 solve :: T.Text -> IO (Int, Int)
 solve input = return (part1, part2)
   where
-    part1 = score $ common . split <$> lines'
-    part2 = score $ common <$> chunksOf 3 lines'
-    lines' = T.lines input
-    common = head . S.toList . foldl1 S.intersection . map (S.fromList . T.unpack)
-    split line = T.chunksOf (T.length line `div` 2) line
+    part1 = score $ head . uncurry intersect . split <$> lines'
+    part2 = score $ head . foldl1 intersect <$> chunksOf 3 lines'
+    lines' = T.unpack <$> T.lines input
+    split line = splitAt (length line `div` 2) line
     score = sum . map priority
-    priority x
-      | isAsciiLower x = ord x - ord 'a' + 1
-      | isAsciiUpper x = ord x - ord 'A' + 27
+    priority item
+      | isAsciiLower item = ord item - ord 'a' + 1
+      | isAsciiUpper item = ord item - ord 'A' + 27
       | otherwise = undefined
