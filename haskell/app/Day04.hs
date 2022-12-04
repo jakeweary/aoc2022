4f3c2b65af78ef23d53e11957212855ebc912677
me <literally@me>
2022-12-04 14:00:05 +0300

    sync

--- haskell/app/Day04.hs
+++ haskell/app/Day04.hs
@@ -6,15 +6,19 @@ import Data.Text qualified as T
 solve :: T.Text -> IO (Int, Int)
 solve input = return (part1, part2)
   where
-    part1 = sum . map fromEnum $ uncurry fullOverlap <$> lines'
-    part2 = sum . map fromEnum $ uncurry someOverlap <$> lines'
-
-    fullOverlap (a, b) (x, y) = overlap (a, b) (x, y) == min (b - a) (y - x)
-    someOverlap (a, b) (x, y) = overlap (a, b) (x, y) /= 0
-    overlap (a, b) (x, y) = max 0 (min b y - max a x)
-
+    part1 = count $ uncurry fullOverlap <$> lines'
+    part2 = count $ uncurry someOverlap <$> lines'
+    count = sum . map fromEnum
     lines' = toRanges . parseLine <$> T.lines input
       where
         parseLine = map (read @Int) . splitOneOf ",-" . T.unpack
         toRanges [a, b, x, y] = ((a, b + 1), (x, y + 1))
         toRanges _ = undefined
+
+fullOverlap, someOverlap :: (Ord a, Num a) => (a, a) -> (a, a) -> Bool
+fullOverlap a b = overlap a b == maxPossibleOverlap a b
+someOverlap a b = overlap a b /= 0
+
+maxPossibleOverlap, overlap :: (Ord a, Num a) => (a, a) -> (a, a) -> a
+maxPossibleOverlap (a, b) (x, y) = min (b - a) (y - x)
+overlap (a, b) (x, y) = max 0 (min b y - max a x)
