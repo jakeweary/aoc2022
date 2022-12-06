edbbafcea5287de6075c39c4b43bf94c8c169f64
me <literally@me>
2022-12-06 15:09:58 +0300

    sync

---
+++ haskell/app/Day06.hs
@@ -0,0 +1,13 @@
+module Day06 (solve) where
+
+import Control.Applicative (ZipList (..))
+import Control.Arrow (Arrow (..))
+import Data.List (findIndex, nub, tails)
+import Data.Maybe (fromJust)
+import Data.Text qualified as T
+
+solve :: T.Text -> IO (Int, Int)
+solve input = return $ (marker 4 &&& marker 14) input
+  where
+    marker n = (+ n) . fromJust . findIndex ((== n) . length . nub) . windows n
+    windows n = getZipList . traverse ZipList . take n . tails . T.unpack . T.stripEnd
--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -6,12 +6,14 @@ import Text.Printf (printf)
 import Day03 qualified
 import Day04 qualified
 import Day05 qualified
+import Day06 qualified
 
 main :: IO ()
 main = do
   day Day03.solve "day03"
   day Day04.solve "day04"
   day Day05.solve "day05"
+  day Day06.solve "day06"
   where
     day solve name = do
       input <- T.readFile $ ".input/" ++ name
