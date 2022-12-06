2c8371d962e0a3ce40cd57dc8d32a79cc73f0d0f
me <literally@me>
2022-12-06 16:11:50 +0300

    sync

--- haskell/app/Day06.hs
+++ haskell/app/Day06.hs
@@ -7,7 +7,7 @@ import Data.Maybe (fromJust)
 import Data.Text qualified as T
 
 solve :: T.Text -> IO (Int, Int)
-solve input = return $ (marker 4 &&& marker 14) input
+solve = return . (marker 4 &&& marker 14)
   where
     marker n = (+ n) . fromJust . findIndex ((== n) . length . nub) . windows n
     windows n = getZipList . traverse ZipList . take n . tails . T.unpack . T.stripEnd
